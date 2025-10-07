110f9b7d804839e4c8ca66fd170dede95e549403
andymalm-ux <andreas.malmgren@student.nbi-handelsakademin.se>
2025-10-07 13:28:38 +0200

    Added class and constructor.

---
+++ Socks.cs
@@ -0,0 +1,16 @@
+
+
+public class Socks
+{
+  public string Type;
+  public string Length;
+  public string Material;
+
+  public Socks(string type, string lenght, string material)
+  {
+    Type = type;
+    Lenght = lenght;
+    Material = material;
+  }
+
+}
